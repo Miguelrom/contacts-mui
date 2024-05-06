89ba9a843739a9a932050718bb60a0b77935ae8e
Miguel Romero <Miguelrom@users.noreply.github.com>
2024-05-06 16:47:44 -0700

    Add title, icons and instructions to the Home page

---
+++ src/components/Instructions.jsx
@@ -0,0 +1,29 @@
+import { Box, Typography } from "@mui/material";
+
+export default function Instructions() {
+  return (
+    <Box>
+      <Typography variant="h4" component="h2">
+        Instructions
+      </Typography>
+      <Box component="ul" sx={{ fontSize: 20, lineHeight: 2}} >
+        <li>
+          To browse all available contacts go to the Contacts menu option and
+          use the table pagination controls.
+        </li>
+        <li>
+          To to see the full information about a given contact click on its
+          corresponding row in the table.
+        </li>
+        <li>
+          To edit or delete a contact click on its corresponding row and click
+          on the EDIT or DELETE button.
+        </li>
+        <li>
+          To create a new contact go to the Contacts menu option and click on
+          the New button.
+        </li>
+      </Box>
+    </Box>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
+import { Typography, Box } from "@mui/material";
+import PhoneIcon from '@mui/icons-material/Phone';
+import ContactMailIcon from '@mui/icons-material/ContactMail';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import Instructions from "../components/Instructions";
 
 export default function Home() {
   return (
     <>
-      <h1>Home page</h1>
+      <Typography align="center" variant="h3" component="h1" sx={{ marginBottom: 10 }} >
+        Welcome to your Contacts!
+      </Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', maxWidth: 500, marginBottom: 10, mx: 'auto'}} >
+        <PhoneIcon sx={{ fontSize: 60 }} />
+        <ContactMailIcon sx={{ fontSize: 60 }} />
+        <AlternateEmailIcon sx={{ fontSize: 60 }} />
+      </Box>
+      <Instructions />
     </>
   );
 }
