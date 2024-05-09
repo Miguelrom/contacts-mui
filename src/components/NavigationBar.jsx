edc63fdf2eee2eefba5d7f21e8a246f21028da46
Miguel Romero <Miguelrom@users.noreply.github.com>
2024-05-09 15:18:35 -0700

    Fix selectedTab updates in NavigationBar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,23 +1,37 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import { Tabs, Tab } from '@mui/material';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, matchPath, useLocation } from 'react-router-dom';
+
+const getSelectedTab = (pathname) => {
+
+  if (matchPath({ path: '/contacts', end: false}, pathname)) {
+    return '/contacts';
+  } else if (matchPath('/about', pathname)) {
+    return '/about';
+  }
+
+  return '/';
+}
 
 export default function NavigationBar() {
 
-  const location = useLocation();
-  const [selectedTab, setSelectedTab] = useState(location.pathname);
+  const { pathname } = useLocation();
 
-  const handleTabChange = (event, value) => {
+  const [selectedTab, setSelectedTab] = useState('/');
 
-    setSelectedTab(value);
-  }
+  useEffect(() => {
+
+    setSelectedTab(getSelectedTab(pathname));
+
+  }, [pathname]);
+
 
   return (
       <AppBar position="sticky" sx={{ backgroundColor: 'white' }}>
         <Toolbar sx={{  }}>
-          <Tabs value={selectedTab} onChange={handleTabChange} >
+          <Tabs value={selectedTab}  >
             <Tab label='Home' value='/' component={Link} to='' />
             <Tab label='Contacs' value='/contacts' component={Link} to='contacts' />
             <Tab label='About' value='/about' component={Link} to='about' />
